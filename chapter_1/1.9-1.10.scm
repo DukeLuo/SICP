ee28b973c1527c883b1fc0bf228fe90973feae9c
duke <dukeluo@outlook.com>
2017-04-26 23:36:47 +0800

    update 1.9-1.10

--- chapter_1/1.9-1.10.scm
+++ chapter_1/1.9-1.10.scm
@@ -1,10 +1,10 @@
 #lang sicp
 
 ;;; recursion
-;(define (factorial n)
-;  (if (= n 1)
-;      1
-;      (* n (factorial (- n 1)))))
+(define (factorial n)
+  (if (= n 1)
+      1
+      (* n (factorial (- n 1)))))
 
 
 ;;; iteration
