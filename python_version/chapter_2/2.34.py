f3a9533fda311075c7f2e7c3fa76e5824e876e27
duke <dukeluo@outlook.com>
2017-06-22 21:52:50 +0800

    add 2.33-2.34 py

---
+++ python_version/chapter_2/2.34.py
@@ -0,0 +1,7 @@
+#! usr/bin/env python
+# -*- coding: utf-8 -*-
+# Exercise 2.34
+
+def horner_eval(x,coefficient_sequence):
+	return accumulate(lambda this_coeff,higher_terms:this_coeff + x * higher_terms,\
+	                  0,coefficient_sequence)
