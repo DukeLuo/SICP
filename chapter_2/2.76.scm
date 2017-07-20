ff9000da7852f6f73ed28e2001ff784848045824
duke <dukeluo@outlook.com>
2017-07-20 20:40:35 +0800

    add 2.75-2.76 scm

---
+++ chapter_2/2.76.scm
@@ -0,0 +1,6 @@
+#lang scheme
+;;; Exercise 2.76
+
+;;; it seems a good rule of thumb would be to use a data-directed style when lots of new operations are required
+;;; and either message-passing or data-directed when lots of new data types are added
+
